d8051733e53144c765095f16af06f5ef390d92ab
Veselin Kolev <vesso.kolev@gmail.com>
2019-02-14 12:28:05 -0800

    Cleaning the code

--- modules/mod_rotate.f90
+++ modules/mod_rotate.f90
@@ -1,161 +1,70 @@
 module mod_rotate
 !
-! This module supports the rotation of the sidechain branches
-! around selected axis and computes the proper dihedral angles.
+! Module supporting the rotation of vectors in 3D.
 !
 ! Author : Veselin Kolev <vesso.kolev@gmail.com>
-! Version: 2019020600
+! Version: 2019021300
 ! License: GPLv2
 !
-
 use iso_c_binding,only:C_INT,C_FLOAT
 
 implicit none
-
-public :: select_branches_to_rotate
-public :: rotate_branch_around_axis
-public :: norm02
-public :: compute_pdih
-public :: rotate_vect_around_axis
-public :: get_rodrigues_matrix
-public :: vector_cross_product
 
 
 contains
 
 
-subroutine select_branches_to_rotate(coord,branches,branches_i,center,&
-                                     cutoff,is_rotatable,num_rotatable)
+subroutine rotate_branch_around_axis(axis,branch,angle,coord)
 !
-! Selects the branches falling within some "cutoff" from "center"
-! (both "cutoff" and "center" are defined by the user). Only the
-! branches having at least one of their atoms within the "cutoff"
-! distance are marked as rotatable (is_rotatable(x)=.true. for them).
+! Rotates a set of points (branch) about an arbitrary axis in 3D.
 !
-! Interface variables:
+! axis - the indexes of the points defining the axis (the
+!        axis is a 3D line) - two point indexes are needed,
+!        and therefore the size of 1D array 'axis' is 2
 !
-real(C_FLOAT),intent(in)   :: coord(:,:)
-integer(C_INT),intent(in)  :: branches(:)
-integer(C_INT),intent(in)  :: branches_i(:,:)
-real(C_FLOAT),intent(in)   :: center(3)
-real(C_FLOAT),intent(in)   :: cutoff
-logical,intent(out)        :: is_rotatable(:)
-integer(C_INT),intent(out) :: num_rotatable
+! branch - 1D array of integers, which are the indexes of the
+!          points to rotate
 !
-! Local variables:
+! angle - the angle of rotation (IN RADIANS!!!)
 !
-integer(C_INT)             :: i
-integer(C_INT)             :: j
-real(C_FLOAT)              :: cutoff_
-
-cutoff_=cutoff**2
+! coord - 3 x num_points - shaped array of floating point real
+!         numbers, containing the coordinates of each point of
+!         the set in 3D space of coordinates
 !
-is_rotatable=.false.
-!
-num_rotatable=0
-!
-! If one atom of a given branch is fount to be rotatable, mark the
-! whole branch as rotatable too.
-!
-do i=1,ubound(branches_i,2)
-   !
-   do j=branches_i(1,i),branches_i(2,i)
-      !
-      ! Use the square of the cutoff. That saves CPU time for not
-      ! calling the 'sqrt' intrinsic function (that function is
-      ! not fast enough).
-      !
-      if (sum((coord(:,branches(j))-center)**2)<cutoff_) then
-         !
-         is_rotatable(i)=.true.
-         !
-         num_rotatable=num_rotatable+1
-         !
-         exit
-         !
-      end if
-      !
-   end do
-   !
-end do
-
-end subroutine select_branches_to_rotate
-
-
-subroutine rotate_branch_around_axis(coord,axes,branches,branches_i,sel,&
-                                     angle,bak_c)
-!
-! Rotates a branch of points around an axis.
-!
-! Both branch members and two atoms defining the axis, have their
-! coordinates listed in the array "coord" (a 3xN array of floats).
-! The array "axes" contains all pairs of indexes in "coord",
-! that can define an axis. The variable "sel" point to the selected
-! axis. Every branch contains the list of indexes of elements of
-! "coord" that should be rotated around the selected axis. The array
-! "branches_i" (containing 2 integer numbers), specifies the range
-! of "branches" elements that point to the indexes of the elements of
-! "coord", participating in the rotation process.
+! NOTES: branch(i) points to coord(:,branch(i))
+!        axis(1) points to coord(:,axis(1))
+!        axis(2) points to coord(:,axis(2))
 !
 ! Interface variables:
 !
-real(C_FLOAT),intent(inout) :: coord(:,:)
-integer(C_INT),intent(in)   :: axes(:,:)
-integer(C_INT),intent(in)   :: branches(:)
-integer(C_INT),intent(in)   :: branches_i(:,:)
-integer(C_INT),intent(in)   :: sel
+integer(C_INT),intent(in)   :: axis(:)
+integer(C_INT),intent(in)   :: branch(:)
 real(C_FLOAT),intent(in)    :: angle
-real(C_FLOAT),intent(out)   :: bak_c(:,:)
+real(C_FLOAT),intent(inout) :: coord(:,:)
 !
 ! Local variables:
 !
 integer(C_INT)              :: i
-real(C_FLOAT)               :: rodrigues_matrix(3,3)
+real(C_FLOAT)               :: matrix(3,3)
 
-call get_rodrigues_matrix(coord(:,axes(1,sel))-coord(:,axes(2,sel)),&
-                          angle,rodrigues_matrix)
+call get_rodrigues_matrix(coord(:,axis(2))-&
+                          coord(:,axis(1)),&
+                          angle,matrix)
 !
-do i=branches_i(1,sel),branches_i(2,sel)
+do i=1,ubound(branch,1)
    !
-   bak_c(:,i-branches_i(1,sel)+1)=coord(:,branches(i))
+   coord(:,branch(i))=coord(:,branch(i))-coord(:,axis(1))
    !
-   coord(:,branches(i))=coord(:,branches(i))-coord(:,axes(1,sel))
+   call rotate_around_axis(coord(:,branch(i)),matrix)
    !
-   call rotate_vect_around_axis(coord(:,branches(i)),rodrigues_matrix)
-   !
-   coord(:,branches(i))=coord(:,branches(i))+coord(:,axes(1,sel))
+   coord(:,branch(i))=coord(:,branch(i))+coord(:,axis(1))
    !
 end do
 
 end subroutine rotate_branch_around_axis
 
 
-subroutine restore_branch(coord,branches,branches_i,sel,bak_c)
-!
-! The subroutine restores the coordinates of the atoms, previously
-! rotated around the axis defined by "axes(:,sel)". Note that the array
-! "bak_c" contains the coordinates of the atoms before the rotation
-! to take place.
-!
-! Interface variables:
-!
-real(C_FLOAT),intent(inout) :: coord(:,:)
-integer(C_INT),intent(in)   :: branches(:)
-integer(C_INT),intent(in)   :: branches_i(:,:)
-integer(C_INT),intent(in)   :: sel
-real(C_FLOAT),intent(in)    :: bak_c(:,:)
-!
-! Local variables:
-!
-integer(C_INT)              :: i
-
-forall (i=branches_i(1,sel):branches_i(2,sel)) coord(:,branches(i))=&
-    bak_c(:,i-branches_i(1,sel)+1)
-
-end subroutine restore_branch
-
-
-subroutine rotate_vect_around_axis(vect,rodrigues_matrix)
+subroutine rotate_around_axis(vect,rodrigues_matrix)
 !
 ! Rotates 3D vector around 3D axis.
 !
@@ -170,28 +79,26 @@ real(C_FLOAT),intent(in)    :: rodrigues_matrix(3,3)
 !
 ! Local variables:
 !
-real(C_FLOAT)               :: rotated(3)
 integer(C_INT)              :: i
 
-forall (i=1:3) rotated(i)=sum(rodrigues_matrix(:,i)*vect(:))
-!
-vect=rotated(:)
+forall (i=1:3) vect(i)=sum(rodrigues_matrix(:,i)*vect(:))
 
-end subroutine rotate_vect_around_axis
+end subroutine rotate_around_axis
 
 
 subroutine get_rodrigues_matrix(vect,angle,matrix)
-
+!
 ! Calculates the rotation matrix elements that allow the
-! a pplication of the Rodrigues' formula. For more details see:
+! application of the Rodrigues' formula. For more details see:
+!
 ! https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
-
+!
 ! NOTES: "vect" defines the 3D equation of the axis line.
 !        It MUST begin at (0,0,0). Supply the angle "angle"
 !        in radians!
-
+!
 ! IMPORTANT!!!
-
+!
 ! The matrix indexing foolows the Fortran way, not the
 ! one of C. Do take this into account!
 !
@@ -218,139 +125,23 @@ real(C_FLOAT)             :: n(3)
 norm=vect/norm02(vect)
 !
 cos_=cos(angle)
-!
 sin_=sin(angle)
 !
 omcos_=1-cos_
 !
 forall (i=1:3) n(i)=norm(factor_1(1,i))*norm(factor_1(2,i))
 !
-do i=1,3
-   !
-   matrix(i,i)=cos_+omcos_*norm(i)**2
-   !
-   matrix(factor_2(1,i),factor_2(2,i))=n(factor_3(1,i))*omcos_-&
-                                       norm(factor_3(2,i))*sin_
-   !
-   matrix(factor_4(1,i),factor_4(2,i))=n(factor_5(1,i))*omcos_+&
-                                       norm(factor_5(2,i))*sin_
-   !
-end do
+forall (i=1:3) matrix(i,i)=cos_+omcos_*norm(i)**2
+!
+forall (i=1:3) matrix(factor_2(1,i),factor_2(2,i))=&
+                  n(factor_3(1,i))*omcos_-&
+                  norm(factor_3(2,i))*sin_
+!
+forall (i=1:3) matrix(factor_4(1,i),factor_4(2,i))=&
+                  n(factor_5(1,i))*omcos_+&
+                  norm(factor_5(2,i))*sin_
 
 end subroutine get_rodrigues_matrix
-
-
-function compute_pdih(coord,i,j,k,l) result(angle)
-!
-! Computes the proper dihedral angle by using 'atan2'.
-! The result is in radians and single precision. It
-! is located within [-pi,pi].
-!
-! Interface variables:
-!
-real(C_FLOAT),intent(in)  :: coord(:,:)
-integer(C_INT),intent(in) :: i
-integer(C_INT),intent(in) :: j
-integer(C_INT),intent(in) :: k
-integer(C_INT),intent(in) :: l
-real(C_FLOAT)             :: angle
-!
-! Local variables:
-!
-real(C_FLOAT)             :: b1(ubound(coord,1))
-real(C_FLOAT)             :: b2(ubound(coord,1))
-real(C_FLOAT)             :: b3(ubound(coord,1))
-real(C_FLOAT)             :: n1(ubound(coord,1))
-real(C_FLOAT)             :: n2(ubound(coord,1))
-real(C_FLOAT),parameter   :: pix2=8*atan(1.0)
-
-b1=coord(:,i)-coord(:,j)
-!
-b2=coord(:,j)-coord(:,k)
-!
-b3=coord(:,k)-coord(:,l)
-!
-! Compute n1 =  b1 x b2
-!
-n1=vector_cross_product(b1,b2)
-!
-! Normalize n1:
-!
-n1=n1/norm02(n1)
-!
-! Note that after n1 is computed there is no need to keep b1
-! anymore. That means b1 might be used as a dummy variable to help
-! reducing the number of declared variables.
-!
-! Compute n2 =  b2 x b3
-!
-n2=vector_cross_product(b2,b3)
-!
-! Normalize n2:
-!
-n2=n2/norm02(n2)
-!
-! Normalize b2:
-!
-b2=b2/norm02(b2)
-!
-! Bellow this line b1 is used as a dummy vector variable. So it
-! can be employed in the product:
-!
-! m1 = n1 x b2
-!
-b1=vector_cross_product(n1,b2)
-!
-! Compute the dihedral angle using atan2:
-!
-angle=atan2(dot_product(b1,n2),dot_product(n1,n2))
-
-end function compute_pdih
-
-
-function norm02(vector) result(res)
-!
-! This function computes the norm of a vector in 3D. The result is
-! single precisions floating point number.
-!
-! NOTICE: Some compilers (GCC gfortran, Intel Fortran) support the
-!         intrinsic function 'norm2'. Unfortunally, that function
-!         is not implemented in other compilers, like those by PGI.
-!
-! Interface variables:
-!
-real(C_FLOAT),intent(in) :: vector(:)
-real(C_FLOAT)            :: res
-
-! Total vectorization (implicit indexing):
-!
-res=sqrt(sum(vector**2))
-
-end function norm02
-
-
-function vector_cross_product(v1,v2) result(prod)
-!
-! Computes the cross product of two 3D vectors.
-! The index selection is based on the 3D Levi-Civita
-! symbol.
-!
-! Interface varirables:
-!
-real(C_FLOAT),intent(in) :: v1(:)
-real(C_FLOAT),intent(in) :: v2(:)
-real(C_FLOAT)            :: prod(ubound(v2,1))
-!
-! Local variables:
-!
-integer(C_INT),parameter :: factor1(3)=(/2,3,1/)
-integer(C_INT),parameter :: factor2(3)=(/3,1,2/)
-
-! Total vectorization (implicit indexing):
-!
-prod=v1(factor1)*v2(factor2)-v1(factor2)*v2(factor1)
-!
-end function vector_cross_product
 
 
 subroutine angle_between_vectors(v1,v2,angle,vprod)
@@ -368,12 +159,105 @@ real(C_FLOAT),intent(in)  :: v2(3)
 real(C_FLOAT),intent(out) :: angle
 real(C_FLOAT),intent(out) :: vprod(3)
 
-vprod=vector_cross_product(v1,v2)
+call vector_cross_product(v1,v2,vprod)
 !
 angle=atan2(sqrt(dot_product(vprod,vprod)),dot_product(v1,v2))
 
 end subroutine angle_between_vectors
 
 
-end module mod_rotate
+subroutine vector_cross_product(v1,v2,prod)
+!
+! Computes the cross product of two vectors in 3D.
+!
+! Interface varirables:
+!
+real(C_FLOAT),intent(in)  :: v1(3)
+real(C_FLOAT),intent(in)  :: v2(3)
+real(C_FLOAT),intent(out) :: prod(3)
+!
+! Local variables:
+!
+integer(C_INT)            :: i
+integer(C_INT),parameter  :: factor1(3)=(/2,3,1/)
+integer(C_INT),parameter  :: factor2(3)=(/3,1,2/)
 
+prod(:)=v1(factor1(:))*v2(factor2(:))-v1(factor2(:))*v2(factor1(:))
+
+end subroutine vector_cross_product
+
+
+function norm02(argument) result(res)
+!
+! This function computes the norm of the N-dimensional
+! vector of floating point numbers.
+!
+! Interface variables:
+!
+real(C_FLOAT),intent(in)  :: argument(:)
+real(C_FLOAT)             :: res
+
+res=sqrt(sum(argument**2))
+
+end function norm02
+
+
+subroutine compute_rot_m(axis_num,angle,matrix)
+!
+! Computes the components of the matrix supporting the rotation
+! of a vector about the axis x, y, or z:
+!
+! axis_num=1 -> x
+! axis_num=2 -> y
+! axis_num=3 -> z
+!
+! by angle 'angle' (given in radians).
+!
+! NOTE: The indexes of the matrix follow the Fortran way of
+!       accessing the memory. That means the matrix defined
+!       bellow is the transpose of the rotation matrix given
+!       in the books.
+!
+! Interface variables:
+!
+integer(C_INT),intent(in) :: axis_num
+real(C_FLOAT),intent(in)  :: angle
+real(C_FLOAT),intent(out) :: matrix(3,3)
+!
+! Local variables:
+!
+integer(C_INT)            :: i
+integer(C_INT)            :: j
+real(C_FLOAT)             :: arr(5)
+integer(C_INT),parameter  :: factor(3,3,3)=[&
+                                  reshape([2,1,1,1,3,5,1,4,3],[3,3]),&
+                                  reshape([3,1,4,1,2,1,5,1,3],[3,3]),&
+                                  reshape([3,5,1,4,3,1,1,1,2],[3,3])]
+
+arr(1)=0.0
+arr(2)=1.0
+!
+! Compute the sin and cos in advance and save the results as
+! local variables (reduces the CPU cost).
+!
+arr(3)=cos(angle)
+arr(4)=sin(angle)
+arr(5)=-arr(4)
+!
+! Note, that the second index of 'matrix' specifies the row number
+! of the rotation matrix (see the NOTE remarks given above):
+!
+do j=1,3
+   !
+   do i=1,3
+      !
+      matrix(i,j)=arr(factor(i,j,axis_num))
+      !
+   end do
+   !
+end do
+
+end subroutine compute_rot_m
+
+
+end module mod_rotate
